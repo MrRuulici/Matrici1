85029549f7e1d32177c4d57a5540308f2aec6d6f
Raul Ulici <raululici@yahoo.com>
2021-12-02 16:45:29 +0200

    Final project

--- Matrici1/Program.cs
+++ Matrici1/Program.cs
@@ -6,7 +6,33 @@ namespace Matrici1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = Int32.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, n];
+
+            bool Identitate = true;
+            for(int i=0;i<matrix.GetLength(0);i++)
+            {
+                for(int j=0;j < matrix.GetLength(1);j++)
+                {
+                    Console.Write($"[{i},{j}]= ");
+                    matrix[i,j] = Int32.Parse(Console.ReadLine());
+                    if(i!=j)
+                    {
+                        if(matrix[i,j] != 0)
+                        {
+                            Identitate = false;
+                        }
+                    }
+                    else if (matrix[i, j] != 1)
+                    {
+                        Identitate = false;
+                    }
+                }
+            }
+            if (Identitate == true)
+                Console.WriteLine("Este matrice identitate");
+            else Console.WriteLine("Nu este matrice identitate");
+
         }
     }
 }
